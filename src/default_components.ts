1d438ff39161dcc932de1baf63c14f59561fa115
Eliott Gaboreau <eliott.gab@gmail.com>
2024-05-28 22:07:56 +0200

    initial commit, only handle paragraphs

---
+++ src/default_components.ts
@@ -0,0 +1,12 @@
+
+import Paragraph from "./components/paragraph.astro";
+import Text from "./components/text.astro";
+import Unknown from "./components/unknown.astro";
+
+
+
+export const defaultComponents = {
+    paragraph: Paragraph,
+    text: Text,
+    unknown: Unknown,
+}
