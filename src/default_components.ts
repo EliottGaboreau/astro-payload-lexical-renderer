4129fd8072ad0610e21cdc98ddcfcdcc2e084dba
Eliott Gaboreau <eliott.gab@gmail.com>
2024-05-28 22:37:09 +0200

    add root node

--- src/default_components.ts
+++ src/default_components.ts
@@ -1,11 +1,13 @@
 
 import Paragraph from "./components/paragraph.astro";
+import Root from "./components/root.astro";
 import Text from "./components/text.astro";
 import Unknown from "./components/unknown.astro";
 
 
 
 export const defaultComponents = {
+    root: Root,
     paragraph: Paragraph,
     text: Text,
     unknown: Unknown,
