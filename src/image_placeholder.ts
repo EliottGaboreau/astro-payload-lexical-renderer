b10093d07cb207c1da90a38b9348e79bc2ff573f
Eliott Gaboreau <eliott.gab@gmail.com>
2024-06-07 15:25:27 +0200

    ntm

--- src/image_placeholder.ts
+++ src/image_placeholder.ts
@@ -6,8 +6,6 @@ import Sharp from "sharp";
 export async function getBase64Placeholder(url: string) {
     const sharp = Sharp('.' + url);
 
-    console.log(sharp);
-
     const buffer = await sharp.toBuffer();
 
     const plaiceholder = await getPlaiceholder(buffer, { size: 8 });
