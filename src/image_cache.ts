6ca0247c0ce92b78b0d9626e572d296ce62f0d57
Eliott Gaboreau <eliott.gab@gmail.com>
2025-02-05 11:57:28 +0100

    pq j'utilise encore ce truc

--- src/image_cache.ts
+++ src/image_cache.ts
@@ -5,7 +5,7 @@ import Sharp from "sharp";
 
 export async function getCachedImage(image: any): Promise<string> {
     const directory = "./src/assets";
-    const name = image.filename || 'undefined';
+    const name = image?.filename || 'undefined';
     const filename = `${name.split('.').slice(0, -1).join('.')}.webp`;
     const path = `${directory}/${filename}`;
 
